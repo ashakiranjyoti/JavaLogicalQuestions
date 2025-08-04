a7311fc24cfe13ae2222380c272302c1743024a5
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-08-04 10:51:08 +0530

    Update Q13_SwapNum.java

--- Q13_SwapNum.java
+++ Q13_SwapNum.java
@@ -1,1 +1,21 @@
+// This class is used to demonstrate swapping two numbers without using a third (temporary) variable
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int a = 10;
+        int b = 20;
+
+        // Print original values before swapping
+        System.out.println("a is " + a + " and b is " + b);
+
+        // Swap logic without using a temporary variable
+        a = a + b; // a becomes 30 (10 + 20)
+        b = a - b; // b becomes 10 (30 - 20), original value of 'a'
+        a = a - b; // a becomes 20 (30 - 10), original value of 'b'
+
+        // Print values after swapping
+        System.out.println("After swapping, a is " + a + " and b is " + b);
+    }
+}
 
