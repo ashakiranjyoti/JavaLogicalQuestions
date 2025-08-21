799f587bf03836732dee2387ab89302af3645ce5
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-08-21 18:02:58 +0530

    Update Q21_InterfaceFeaturesDemo.java

--- Q21_InterfaceFeaturesDemo.java
+++ Q21_InterfaceFeaturesDemo.java
@@ -2,7 +2,7 @@ public class InterfaceDemo {
     public static void main(String[] args) {
         Interface1 obj = new MyClass();
 
-        
+        // Call abstract  (implemented by MyClass)
         obj.method1("Hello from method1");
 
         // Call default method (overridden in MyClass)
