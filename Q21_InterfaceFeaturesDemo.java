3dc24eb3be066b41916f4679f8504c5730a976e8
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-08-21 18:02:33 +0530

    Update Q21_InterfaceFeaturesDemo.java

--- Q21_InterfaceFeaturesDemo.java
+++ Q21_InterfaceFeaturesDemo.java
@@ -1,1 +1,21 @@
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Interface1 obj = new MyClass();
+
+        
+        obj.method1("Hello from method1");
+
+        // Call default method (overridden in MyClass)
+        obj.log("Testing log");
+
+        // Call static method from interface
+        boolean result1 = Interface1.isNull(null);
+        boolean result2 = Interface1.isNull("");
+        boolean result3 = Interface1.isNull("Hello");
+
+        System.out.println("isNull(null) = " + result1);   // true
+        System.out.println("isNull(\"\") = " + result2);    // true
+        System.out.println("isNull(\"Hello\") = " + result3); // false
+    }
+}
 
