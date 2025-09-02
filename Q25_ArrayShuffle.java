3fae0c67de2d4556acee3d9036309e31de2c7622
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-02 10:26:16 +0530

    Update Q25_ArrayShuffle.java

--- Q25_ArrayShuffle.java
+++ Q25_ArrayShuffle.java
@@ -1,1 +1,27 @@
+import java.util.Arrays; // For printing the array
+import java.util.Random; // For generating random numbers
+
+public class ArrayShuffle {
+    public static void main(String[] args) {
+
+        int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+
+        
+        Random rand = new Random();
+
+
+        for (int i = 0; i < array.length; i++) {
+            // Generate a random index within the bounds of the array
+            int randomIndexToSwap = rand.nextInt(array.length);
+
+            // Swap the current element with the randomly chosen element
+            int temp = array[randomIndexToSwap];
+            array[randomIndexToSwap] = array[i];
+            array[i] = temp;
+        }
+
+        // Print the shuffled array
+        System.out.println(Arrays.toString(array));
+    }
+}
 
