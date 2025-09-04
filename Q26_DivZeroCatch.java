a25db96409d136b809c06f042d6607dc19a2ebfb
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-04 10:04:14 +0530

    Update Q26_DivZeroCatch.java

--- Q26_DivZeroCatch.java
+++ Q26_DivZeroCatch.java
@@ -1,1 +1,27 @@
+import java.util.*;  
+
+class Main {
+    public static void main(String args[]) {
+        
+        Scanner scn = new Scanner(System.in);
+
+        
+        int n = scn.nextInt();
+
+        // Informing user that the program will attempt division by 0
+        System.out.println("Dividing this number by 0");
+
+        // Try-catch block to handle the ArithmeticException
+        try {
+            // This line will cause an ArithmeticException (division by zero)
+            System.out.println(n / 0);
+        } catch (Exception e) {
+            // Catching the exception and printing the exception message
+            System.out.println(e);
+        }
+
+        // This line will execute regardless of the exception
+        System.out.println("Program completed");
+    }
+}
 
