334145ebbafd11e9b46a0bd4daf662ff52b308af
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-08 09:58:42 +0530

    Update 30_NoZeroSum.java

--- 30_NoZeroSum.java
+++ 30_NoZeroSum.java
@@ -4,7 +4,7 @@ class Solution {
  // Method to find two non-zero-digit integers whose sum is n
   public int[] getNoZeroIntegers(int n) {
     
-    
+     Loop through possible values of A from 1 to n-1
     for (int A = 1; A < n; ++A) {
       int B = n - A; // Calculate the corresponding B such that A + B = n
 
