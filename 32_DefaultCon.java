c97ac6df44185f1cf97c2fbfd76830eddc6373b7
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-18 13:03:45 +0530

    Update 32_DefaultCon.java

--- 32_DefaultCon.java
+++ 32_DefaultCon.java
@@ -1,3 +1,4 @@
+// Write a Java program to create a class called "Cat" with instance variables name and age. Implement a default constructor that initializes the name to "Unknown" and the age to 0. Print the values of the variables.
 // Define a class named 'Cat'
 class Cat {
     // Declare instance variables (also called fields)
