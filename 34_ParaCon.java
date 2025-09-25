932f539c594485e01bcb6479364dd6784c07b7ca
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-25 10:06:17 +0530

    Update 34_ParaCon.java

--- 34_ParaCon.java
+++ 34_ParaCon.java
@@ -1,7 +1,7 @@
 // Write a Java program to create a class called Dog with instance variables name and color. Implement a parameterized constructor that takes name and color as parameters and initializes the instance variables. Print the values of the variables.
 
 
-
+// Define the Dog class
 public class Dog {
     
     private String name;
