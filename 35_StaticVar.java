be730120b5dc15fceacd3fc2d5ec51e9248abcb1
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-29 10:00:54 +0530

    Update 35_StaticVar.java

--- 35_StaticVar.java
+++ 35_StaticVar.java
@@ -1,1 +1,27 @@
 
+public class Counter {
+    
+    private static int count = 0;
+
+    
+    public Counter() {
+        count++;
+    }
+
+    
+    public static int getCount() {
+        return count;
+    }
+
+    // Main method to test the Counter class
+    public static void main(String[] args) {
+        // Create several Counter objects
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+
+        // Print the value of count
+        System.out.println("Count: " + Counter.getCount());
+    }
+}
+
