202e7a3f52d3f2fa8350367ea8107af7303b9013
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-09-30 10:23:11 +0530

    Update 36_StaticBlock.java

--- 36_StaticBlock.java
+++ 36_StaticBlock.java
@@ -1,6 +1,6 @@
 // Write a Java program to create a class called "Initializer" with a static block that initializes a static variable 'initialValue' to 1000. Print the value of 'initialValue' before and after creating an instance of "Initializer".
 
-// Define the Initializer 
+// Define the Initializer class
 public class Initializer {
     // Declare a static variable initialValue
     static int initialValue;
