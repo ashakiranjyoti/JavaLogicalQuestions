e997a4c886880bbd935432ea2ef8c1845842cdfb
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-10-13 16:15:33 +0530

    Update 38_Inheritance.java

--- 38_Inheritance.java
+++ 38_Inheritance.java
@@ -8,3 +8,13 @@ public class Animal {
         System.out.println("The animal makes a sound");
     }
 } 
+// Define a public class named Cat that extends Animal
+public class Cat extends Animal {
+    // Use the @Override annotation to indicate that this method overrides a method in the superclass
+    @Override
+    // Define the makeSound method
+    public void makeSound() {
+        // Print "The cat quarrels." to the console
+        System.out.println("The cat quarrels.");
+    }
+}
