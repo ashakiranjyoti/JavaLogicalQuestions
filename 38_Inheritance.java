4853af69c25cd081290634ceee187501ea9f06c3
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-10-13 16:15:12 +0530

    Update 38_Inheritance.java

--- 38_Inheritance.java
+++ 38_Inheritance.java
@@ -1,1 +1,10 @@
 // Write a Java program to create a class called Animal with a method called makeSound(). Create a subclass called Cat that overrides the makeSound() method to bark.
+
+// Define a public class named Animal
+public class Animal {
+    // Define a public method named makeSound
+    public void makeSound() {
+        // Print "The animal makes a sound." to the console
+        System.out.println("The animal makes a sound");
+    }
+} 
