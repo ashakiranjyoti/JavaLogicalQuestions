6df17c1fb77b0f2f3eb6a5778576b95122ed6c36
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-10-15 18:46:49 +0530

    Update 39_AnonyClass.java

--- 39_AnonyClass.java
+++ 39_AnonyClass.java
@@ -1,6 +1,6 @@
 // Write a Java program to create an interface called Greeting with a method sayHello(). In the main method, create an anonymous class that implements the Greeting interface and override the sayHello() method to print "Hello, World!". Call the sayHello() method.
 
-
+// Main class containing the main method
 public class Main {
     public static void main(String[] args) {
         // Creating an anonymous class that implements the Greeting interface
