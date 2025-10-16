5b26a4286bf821cfa5a5df2de047596cb117bd3d
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-10-16 18:41:17 +0530

    Update 40_StaticMethod.java

--- 40_StaticMethod.java
+++ 40_StaticMethod.java
@@ -1,3 +1,5 @@
+Write a Java program to create a class called "Initializer" with a static block that initializes a static variable 'initialValue' to 1000. Print the value of 'initialValue' before and after creating an instance of "Initializer".
+
 // Define the Initializer class
 public class Initializer {
     // Declare a static variable initialValue
