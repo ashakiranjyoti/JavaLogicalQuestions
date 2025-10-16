acbf33fa270e58bcb75f6f3623995347306c7106
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-10-16 18:39:20 +0530

    Update 40_StaticMethod.java

--- 40_StaticMethod.java
+++ 40_StaticMethod.java
@@ -1,1 +1,9 @@
+public class Initializer {
+    // Declare a static variable initialValue
+    static int initialValue;
 
+    // Static block to initialize initialValue
+    static {
+        initialValue = 1000;
+        System.out.println("Static block: initialValue initialized to " + initialValue);
+    }
