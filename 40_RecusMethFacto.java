7282e52342fcc5bb43b0a9611e90d512c59a980d
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2025-10-18 12:00:35 +0530

    Update 40_RecusMethFacto.java

--- 40_RecusMethFacto.java
+++ 40_RecusMethFacto.java
@@ -1,4 +1,4 @@
-Write a Java recursive method to calculate the factorial of a given positive integer.
+// Write a Java recursive method to calculate the factorial of a given positive integer.
 
 public class FactorialCalculator {
 
